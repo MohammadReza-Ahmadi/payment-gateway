ad3fb44566c6102fd6196e8b8b264196f07e1844
mohammad-reza <mohammad-reza@mohammad-reza-ubuntu-20.04>
2020-12-09 10:54:19 +0330

    first commit

---
+++ src/main/java/com/vosouq/paymentgateway/mellat/config/WebConfiguration.java
@@ -0,0 +1,18 @@
+package com.vosouq.paymentgateway.mellat.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+//        /**
+        registry.addResourceHandler("/callbacks/ipg/mellat/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
